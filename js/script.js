5cb336680f1e1fc0243f13a0f96565f9d9921977
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-09-15 16:41:13 +0200

    improved Save UX

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 processController = function($scope, $http) {
 
-    var success = function(response) {
-    	console.log(response.data);
-    	jsonObject = angular.fromJson(response.data);
-    	console.log('json=' + response.data);
-    	$scope.notes = jsonObject.notes;
+	var success = function(response) {
+		console.log(response.data);
+		jsonObject = angular.fromJson(response.data);
+		console.log('json=' + response.data);
+		$scope.notes = jsonObject.notes;
 		$scope.booting = false;
-    }
+	}
 
-    var error = function(reason) {
-    	console.log(reason);
-    	$scope.message = 'fel';
+	var error = function(reason) {
+		console.log(reason);
+		$scope.message = 'fel';
 		$scope.booting = false;
-    }
+	}
 
 	$http.get(ENDPOINT_URL + 'list')
-		 .then(success, error);
+	.then(success, error);
 
 	var removeTop = function() {
 		console.log('removeTop');
@@ -28,23 +28,33 @@ processController = function($scope, $http) {
 	$scope.booting = true;
 };
 
-collectController = function($scope, $http) {
+collectController = function($scope, $http, $timeout) {
 
 	var collect = function($note) {
 		console.log('collect');
-        $scope.state = 'saving';
+		$scope.isSaving = true;
+		$scope.message = 'Sparar...';
+		console.log($scope.isSaving);
 		$http({
-		      url: ENDPOINT_URL + 'add',
-		      method: "POST",
-		      headers: { 'Content-Type': 'application/json' },
-		      data: JSON.stringify({note:$note})
-		    }).success(function(data) {
-		      console.log(data)
-		    });
+			url: ENDPOINT_URL + 'add',
+			method: "POST",
+			headers: { 'Content-Type': 'application/json' },
+			data: JSON.stringify({note:$note})
+		}).success(function(data) {
+			$scope.isSaving = false;
+			$scope.message = 'Sparat. :)';
+			$scope.note = '';
+	        $timeout(function(){
+	            $scope.message = '';
+	        }, 2000);
+	        console.log(data)
+		});
 	}
 
 	$scope.collect = collect;
 	$scope.note = '';
+	$scope.isSaving = false;
+	$scope.message= '';
 };
 
 var app = angular.module("remindApp", []);

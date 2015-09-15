9929263e504696cc9b5479f6b5ae8d9ea6517836
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-09-15 12:49:12 +0200

    remove src/ dir in the name of consistency with backend folder structure. added simple landing page index.html, linking to collect.html and process.html.

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ processController = function($scope, $http) {
     	jsonObject = angular.fromJson(response.data);
     	console.log('json=' + response.data);
     	$scope.notes = jsonObject.notes;
+		$scope.booting = false;
     }
 
     var error = function(reason) {
     	console.log(reason);
     	$scope.message = 'fel';
+		$scope.booting = false;
     }
 
 	$http.get(ENDPOINT_URL + 'list')
@@ -23,7 +25,21 @@ processController = function($scope, $http) {
 
 	$scope.removeTop = removeTop;
 	$scope.notes = [];
-}
+	$scope.booting = true;
+};
+
+collectController = function($scope, $http) {
+
+	var collect = function($note) {
+		console.log('collect');
+		$http.post(ENDPOINT_URL + 'add',
+		           { note: $note} );
+	}
+
+	$scope.collect = collect;
+	$scope.note = '';
+};
 
 var app = angular.module("remindApp", []);
 app.controller("processController", processController);
+app.controller("collectController", collectController);
--- src/js/config.js
+++
@@ -1,2 +0,0 @@
-var ENDPOINT_URL = 'http://localhost:5000/';
-//var ENDPOINT_URL = "https://remind-gtd-micro-app.herokuapp.com/";
